f4f2660bf15ab07129d04fa56f7ca9a14f5c860b
Taki Uddin <takiuddin93@gmail.com>
2022-01-28 16:16:36 +0600

    experiance section modified

--- src/experience/experience.component.ts
+++ src/experience/experience.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ExperienceComponent implements OnInit {
 
   ids: any = "vumobile";
+  vumobile = "2px solid #C6C6C4";
+  asiaticjwt = "2px solid #C6C6C4";
+  axionesl = "2px solid #C6C6C4";
+  grameenphone = "2px solid #C6C6C4";
   constructor() { }
 
   ngOnInit(): void {
@@ -15,7 +19,27 @@ export class ExperienceComponent implements OnInit {
 
   workplaceChange(ids: any){
     this.ids = ids;
-    console.log(this.ids);
+    if(ids == "vumobile"){
+      this.vumobile = "3px solid #E88073";
+      this.asiaticjwt = "2px solid #C6C6C4";
+      this.axionesl = "2px solid #C6C6C4";
+      this.grameenphone = "2px solid #C6C6C4";
+    } else if(ids == "asiaticjwt"){
+      this.vumobile = "2px solid #C6C6C4";
+      this.asiaticjwt = "3px solid #E88073";
+      this.axionesl = "2px solid #C6C6C4";
+      this.grameenphone = "2px solid #C6C6C4";
+    } else if(ids == "axionesl"){
+      this.vumobile = "2px solid #C6C6C4";
+      this.asiaticjwt = "2px solid #C6C6C4";
+      this.axionesl = "3px solid #E88073";
+      this.grameenphone = "2px solid #C6C6C4";
+    } else if(ids == "grameenphone"){
+      this.vumobile = "2px solid #C6C6C4";
+      this.asiaticjwt = "2px solid #C6C6C4";
+      this.axionesl = "2px solid #C6C6C4";
+      this.grameenphone = "3px solid #E88073";
+    }
     
   }
 
